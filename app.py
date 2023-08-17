46d9cb868d5d67cc1c9662bf96d804a61e25cbff
Edmund Akogyeram <edakoyeram@gmail.com>
2023-08-17 22:02:43 +0000

    commenting and creating new route

--- app.py
+++ app.py
@@ -3,16 +3,19 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/')
-def home():
-    return '<h1>This is an index page</h1>'
+# @app.route('/')
+# def home():
+#     return '<h1>This is an index page</h1>'
 
-@app.route('/new')
-def new():
-    return '<h1>This is a new page</h1>'
+# @app.route('/new')
+# def new():
+#     return '<h1>This is a new page</h1>'
 
-@app.route('/demo')
-def demo():
-    return '<h1>This is a demo page</h1>'
+# @app.route('/demo')
+# def demo():
+#     return '<h1>This is a demo page</h1>'
+
+
+@app.route('/profile')
 
 app.run()
