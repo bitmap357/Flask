6c2b04ef0e9430ff6d2946ee3796c784e300c446
Edmund Akogyeram <edakoyeram@gmail.com>
2023-08-18 20:39:35 +0000

    making corrections

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def welcome_admin():
     return 'Welcome admin'
 
 @app.route('/guest/<guest>')
-def welcome_guest():
+def welcome_guest(guest):
     return 'Welcome guest, %s' % guest
 
 @app.route('/user/<name>')
@@ -34,7 +34,7 @@ def welcome_user(name):
     if name == 'admin':
         return redirect(url_for('welcome_admin'))
     else:
-        return redirect(url_for('welcome_guest'))
+        return redirect(url_for('welcome_guest', guest=name))
 
 
 app.run(debug=True)
