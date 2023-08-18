5a15a0508e2f083b0c15d3baf009a0955b1b190e
Edmund Akogyeram <edakoyeram@gmail.com>
2023-08-18 20:35:51 +0000

    checking which type of user logged in

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 
 app = Flask(__name__)
 
@@ -28,5 +28,13 @@ def welcome_admin():
 @app.route('/guest/<guest>')
 def welcome_guest():
     return 'Welcome guest, %s' % guest
+
+@app.route('/user/<name>')
+def welcome_user(name):
+    if name == 'admin':
+        return redirect(url_for('welcome_admin'))
+    else:
+        return redirect(url_for('welcome_guest'))
+
 
 app.run(debug=True)
