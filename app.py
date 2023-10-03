fd031350fb5b68fea1ae5b5bfce9f7179828ceea
Edmund Akogyeram <edakoyeram@gmail.com>
2023-10-03 10:57:28 +0000

    ~C

--- app.py
+++ app.py
@@ -11,44 +11,6 @@ app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
 
-# @app.route('/')
-# def home():
-#     return '<h1>This is an index page</h1>'
-
-# @app.route('/new')
-# def new():
-#     return '<h1>This is a new page</h1>'
-
-# @app.route('/demo')
-# def demo():
-#     return '<h1>This is a demo page</h1>'
-
-
-# @app.route('/profile/<int:id>')
-# def profile(id):
-#     return '<h1>This is a profile page for %d</h1>' % id
-
-
-# @app.route('/admin')
-# def welcome_admin():
-#     return 'Welcome admin'
-
-# @app.route('/guest/<guest>')
-# def welcome_guest(guest):
-#     return 'Welcome guest, %s' % guest
-
-# @app.route('/user/<name>')
-# def welcome_user(name):
-#     if name == 'admin':
-#         return redirect(url_for('welcome_admin'))
-#     else:
-#         return redirect(url_for('welcome_guest', guest=name))
-
-
-# @app.route('/')
-# def index():
-#     return 'This is a request made by the client %s' % request.headers
-
 
 @app.route('/addbook')
 def addbook():
