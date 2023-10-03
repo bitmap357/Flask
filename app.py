3dae15eec0def40a96323e4eee9adc5ceacee6cb
Edmund Akogyeram <edakoyeram@gmail.com>
2023-10-03 10:57:02 +0000

    CONNECTING DATABASE FILE TO APP

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ import os
 project_dir = os.path.dirname(os.path.abspath(__file__))
 database_file = "sqlite:///{}".format(
     os.path.join(project_dir, "mydatabase.db"))
+
 app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = database_file
 
 
 # @app.route('/')
