4005bd6a8eeacaa9370aca3bb39ec1a7729b6d21
Edmund Akogyeram <edakoyeram@gmail.com>
2023-10-03 10:53:58 +0000

    database file

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import os
 
 
 project_dir = os.path.dirname(os.path.abspath(__file__))
+database_file = "sqlite:///{}".format(
+    os.path.join(project_dir, "mydatabase.db"))
 app = Flask(__name__)
 
 
